8a704ab4087e7cfcdbb8bc7bbc602ffc2b2c0af5
ejyongc <ejyongc@gmail.com>
2021-05-27 00:18:25 -0500

    added lines to the map

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -2,29 +2,25 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-// Create the map object with a center and zoom level.
-let map = L.map("mapid", {
-    center: [40.7, -94.5],
-    zoom: 4
-  });
+let map = L.map("mapid").setView([37.6213, -122.3790],5);
 
 // Get data from cities.js
 let cityData = cities;
 
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-  console.log(city)
-  L.circleMarker(city.location, {
-    radius: city.population/200000,
-    color: 'orange',
-    weight: '4'
-  })
-  .bindPopup("<h2>" + city.city + "," + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-  .addTo(map);
-});
+// // OLD EXERCISE - Loop through the cities array and create one marker for each city.
+// cityData.forEach(function(city) {
+//   console.log(city)
+//   L.circleMarker(city.location, {
+//     radius: city.population/200000,
+//     color: 'orange',
+//     weight: '4'
+//   })
+//   .bindPopup("<h2>" + city.city + "," + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+//   .addTo(map);
+// });
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -32,3 +28,27 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+// // OLD EXERCISE - Coordinates for each point to be used in the line.
+// let line = [
+//   [33.9416, -118.4085],
+//   [37.6213, -122.3790]
+// ];
+
+// Coordinates for each point to be used in the polyline.
+let line = [
+  // San Francisco Coordinates
+  [37.6213, -122.3790],
+  // Austin Coordinates
+  [30.1975, -97.6664],
+  // Toronto Pearson International Airport coordinates
+  [43.6777, -79.6248],
+  // JFK Kennedy Airport Coodinates
+  [40.6413, -73.7781]
+];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+  color: "#034f84",
+  dashArray: 4
+}).addTo(map);
